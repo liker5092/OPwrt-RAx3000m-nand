b376f887a9b84a02b5f594069fdec50a913cf6ee
liker5092 <31425947+liker5092@users.noreply.github.com>
2024-05-26 16:31:13 +0800

    Update diy1.sh

--- DIY/diy1.sh
+++ DIY/diy1.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #=================================================
-# MZwrt script
+# OPwrt script
 #================================================= 
 
 
 ##添加自己的插件库
-echo -e "\nsrc-git extraipk https://github.com/mzwrt/extra_ipk" >> feeds.conf.default
+echo -e "\nsrc-git extraipk https://github.com/liker5092/extra_ipk" >> feeds.conf.default
